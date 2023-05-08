f3cc8af0950ae110be2701c0a07ac92e7778747b
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2023-05-08 19:45:26 -0300

    requisito13_(12 em andamento)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
 
-export default class Profile extends React.Component {
+class Profile extends Component {
+  state = {
+    isLoading: false,
+    name: '',
+    email: '',
+    image: '',
+    description: '',
+  };
+
+  componentDidMount() {
+    this.handleUser();
+  }
+
+  handleUser = async () => {
+    this.setState({ isLoading: true });
+    const { name, email, image, description } = await getUser();
+    this.setState({ name, email, image, description });
+  };
+
   render() {
+    const { name, email, image, description } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
-        Profile
+        {/* Profile */}
+        <h4>{ name }</h4>
+        <p>{ email }</p>
+        <img data-testid="profile-image" src={ image } alt={ name } />
+        <p>{ description }</p>
+        <Link to="/profile/edit">Editar perfil</Link>
       </div>
     );
   }
 }
+export default Profile;
