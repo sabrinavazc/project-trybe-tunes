be9a2513380b4eb0e82da30335c6594ac459d059
Sabrina Vaz Camêlo <sabrina.vaz.c@gmail.com>
2023-05-08 18:43:22 -0300

    requisito07

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,101 @@
+import PropTypes from 'prop-types';
+import React, { useState, useEffect, useCallback } from 'react';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
+
+function MusicCard(props) {
+  const { trackId, trackName, previewUrl, artworkUrl100 } = props;
+  const [loading, setLoading] = useState(false);
+  const [checked, setChecked] = useState(false);
+  const [favoriteSongs, setFavoriteSongs] = useState([]);
+
+  const handleFavoriteSong = useCallback(async () => {
+    const favoritesSong = await getFavoriteSongs();
+    setChecked(favoritesSong.some(({ trackId: id }) => id === trackId));
+    setFavoriteSongs(favoritesSong);
+  }, [trackId]);
+
+  useEffect(() => {
+    async function getFavoriteSongsAsync() {
+      await handleFavoriteSong();
+    }
+    getFavoriteSongsAsync();
+  }, [handleFavoriteSong]);
+
+  async function handleCheckBox({ target }) {
+    setLoading(true);
+    setChecked(target.checked);
+    if (target.checked) {
+      await addSong({ trackId, trackName, previewUrl, artworkUrl100 });
+      console.log(`Música ${trackName} adicionada aos favoritos!`);
+    } else {
+      await removeSong(trackId);
+      console.log(`Música ${trackName} removida dos favoritos!`);
+    }
+    const favoritesSong = await getFavoriteSongs();
+    setFavoriteSongs(favoritesSong);
+    setLoading(false);
+  }
+
+  async function toggleFavorite() {
+    setLoading(true);
+    if (!favoriteSongs.some(({ trackId: id }) => id === trackId)) {
+      await addSong({ trackId, trackName, previewUrl, artworkUrl100 });
+      console.log(`Música ${trackName} adicionada aos favoritos!`);
+    } else {
+      await removeSong(trackId);
+      console.log(`Música ${trackName} removida dos favoritos!`);
+    }
+    const favoritesSong = await getFavoriteSongs();
+    setFavoriteSongs(favoritesSong);
+    setLoading(false);
+  }
+
+  const checkBoxTestId = `checkbox-music-${trackId}`;
+
+  return (
+    <div>
+      <p>{trackName}</p>
+      <img src={ artworkUrl100 } alt={ trackName } />
+
+      <audio
+        data-testid="audio-component"
+        src={ previewUrl }
+        controls
+      >
+        <track kind="captions" />
+        O seu navegador não suporta o elemento
+        {' '}
+        <code>audio</code>
+        .
+      </audio>
+
+      <button onClick={ toggleFavorite }>
+        {favoriteSongs.some(({ trackId: id }) => id === trackId)
+          ? 'Remover dos favoritos' : 'Adicionar aos favoritos'}
+      </button>
+
+      <label htmlFor={ checkBoxTestId }>
+        Favorita
+        {loading ? <Loading /> : (
+          <input
+            type="checkbox"
+            id={ checkBoxTestId }
+            data-testid={ checkBoxTestId }
+            checked={ checked }
+            onChange={ handleCheckBox }
+          />
+        )}
+      </label>
+    </div>
+  );
+}
+
+MusicCard.propTypes = {
+  artworkUrl100: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+  trackId: PropTypes.number.isRequired,
+  trackName: PropTypes.string.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,91 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
-export default class Album extends React.Component {
+class Album extends Component {
+  state = {
+    artistName: '',
+    collectionName: '',
+    trackList: [],
+  };
+
+  componentDidMount() {
+    this.renderTrackList();
+  }
+
+  renderTrackList = async () => {
+    const { match: { params: { id } } } = this.props;
+    const trackList = await getMusics(id);
+    const { artistName, collectionName } = trackList[0];
+
+    this.setState({
+      artistName,
+      collectionName,
+      trackList,
+    });
+  };
+
+  handleCheckBox = async ({ target }, trackId, trackName, previewUrl) => {
+    const { checked } = target;
+    const { getFavoriteSongs, setChecked, setLoading, addSong, removeSong } = this.props;
+
+    setChecked(getFavoriteSongs().some(({ trackId: id }) => id === trackId));
+    setLoading(true);
+
+    const song = { trackId, trackName, previewUrl };
+    if (checked) {
+      await addSong(song);
+      console.log(`Música ${trackName} adicionada aos favoritos!`);
+    } else {
+      await removeSong(trackId);
+      console.log(`Música ${trackName} removida dos favoritos!`);
+    }
+    setLoading(false);
+  };
+
   render() {
+    const { artistName, collectionName, trackList } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        <div>
+          {
+            trackList.map(({ trackId, trackName, previewUrl }, index) => (
+              index === 0 ? (
+                <div key={ index }>
+                  <h2 data-testid="album-name">{ collectionName }</h2>
+                  <h4 data-testid="artist-name">{ artistName }</h4>
+                </div>
+              ) : (
+                <MusicCard
+                  key={ trackId }
+                  trackId={ trackId }
+                  trackName={ trackName }
+                  previewUrl={ previewUrl }
+                  handleCheckBox={ this.handleCheckBox }
+                />
+              )
+            ))
+          }
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  getFavoriteSongs: PropTypes.func.isRequired,
+  setChecked: PropTypes.func.isRequired,
+  setLoading: PropTypes.func.isRequired,
+  addSong: PropTypes.func.isRequired,
+  removeSong: PropTypes.func.isRequired,
+};
+
+export default Album;
